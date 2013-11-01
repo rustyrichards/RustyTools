cfe6f63c90acd4dfe5a1fa6049d7562aecb86ba7
Rusty Richards <fe3o4y@gmail.com>
2013-11-01 12:38:56 -0400

    Converted to  Mozilla Public License 2.0

--- Lib/rustyToolsUi.js
+++ Lib/rustyToolsUi.js
@@ -1,5 +1,7 @@
-// © 2013 Russell W. Richards
-// License: Not yet determined.
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 
 RustyTools.Ui = {
 	// Strings to use with multiReplace to create components.
--- Lib/rustyToolsXhr__test.js
+++ Lib/rustyToolsXhr__test.js
@@ -1,5 +1,6 @@
-// © 2013 Russell W. Richards
-// License: Not yet determined.
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // The testers have a lot of tiny functons - use the whole script "use strict".
 "use strict";
